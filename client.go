f78bc9c36083ff16927bc8338e2818566acc10bb
EileenG2606 <165683405+EileenG2606@users.noreply.github.com>
2024-05-27 12:44:33 +0700

    Update client.go

--- client.go
+++ client.go
@@ -4,18 +4,19 @@ import (
 	"bufio"
 	"bytes"
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
 	"fmt"
 	"io"
-	"net"
 	"net/http"
 	"os"
 	"time"
 )
 
-func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
+func main(){
+	scanner:= bufio.NewScanner(os.Stdin)
+	for{
 		fmt.Println("1. Receive data (GET)")
 		fmt.Println("2. Send data (POST)")
 		fmt.Printf(">> ")
@@ -25,40 +26,59 @@ func main() {
 			receiveData()
 		} else if choo == "2"{
 			sendData()
-		} else {
+		} else{
 			fmt.Println("Please select 1 or 2 :)")
 		}
 	}
 }
 
-var client = &http.Client{
-	Transport: &http.Transport{
-		DisableKeepAlives: true,
-		DialContext: (&net.Dialer{
-			Timeout:   5 * time.Second,
-			KeepAlive: 5 * time.Second,
-		}).DialContext,
-	},
-	Timeout: 7 * time.Second,
+func loadTLSConfig() (*tls.Config, error){
+	certPool:= x509.NewCertPool()
+	certP,err:= os.ReadFile("cert.pem")
+	if err != nil{
+		return nil, err
+	}
+	certPool.AppendCertsFromPEM(certP)
+	return &tls.Config{
+		RootCAs: certPool,
+	}, nil
+}
+
+var client= &http.Client{
+	Timeout: 7*time.Second,
 }
 
 func receiveData(){
 	ctx, cancel:= context.WithTimeout(context.Background(), 7*time.Second)
 	defer cancel()
 
-	req, err:= http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost:5678", nil)
+	tlsConf,err:= loadTLSConfig()
+	if err!=nil{
+		fmt.Println(err)
+		return
+	}
+
+	// konfigurasi tls diimplementasikan dalan http
+	client.Transport= &http.Transport{
+		TLSClientConfig: tlsConf,
+	}
+
+	req,err:= http.NewRequestWithContext(ctx, http.MethodGet, "https://localhost:5678", nil)
 	if err != nil{
+		fmt.Println(err)
 		return
 	}
 
 	resp, err:= client.Do(req)
-	if err != nil{
+	if err!=nil{
+		fmt.Println(err)
 		return
 	}
 	defer resp.Body.Close()
 
-	dataRe, err:= io.ReadAll(resp.Body)
-	if err != nil{
+	dataRe,err:= io.ReadAll(resp.Body)
+	if err!=nil{
+		fmt.Println(err)
 		return
 	}
 
@@ -72,28 +92,42 @@ func sendData(){
 		"Email": "BB@gmail.com",
 	}
 
-	jsonData, err:= json.Marshal(data)
+	jsonData,err:= json.Marshal(data)
 	if err!=nil{
+		fmt.Println(err)
 		return
 	}
 
-	contx, cancel:= context.WithTimeout(context.Background(), 10*time.Second)
+	contx,cancel:= context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	req, err:= http.NewRequestWithContext(contx, http.MethodPost, "http://localhost:5678/post", bytes.NewBuffer(jsonData))
+	tlsConfig, err := loadTLSConfig()
 	if err!=nil{
+		fmt.Println(err)
+		return
+	}
+
+	client.Transport= &http.Transport{
+		TLSClientConfig: tlsConfig,
+	}
+
+	req, err:= http.NewRequestWithContext(contx, http.MethodPost, "https://localhost:5678/post", bytes.NewBuffer(jsonData))
+	if err!=nil{
+		fmt.Println(err)
 		return
 	}
 	req.Header.Set("Content-Type", "application/json")
 
-	response, err := client.Do(req)
+	response,err:= client.Do(req)
 	if err!=nil{
+		fmt.Println(err)
 		return
 	}
 	defer response.Body.Close()
 
-	content, err:= io.ReadAll(response.Body)
+	content,err:= io.ReadAll(response.Body)
 	if err!=nil{
+		fmt.Println(err)
 		return
 	}
 
